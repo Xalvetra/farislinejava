f58c0c5784bd6aa87a62d72e4219fe351cb32808
Xalvetra <farisikhlasulhaq@yahoo.co.id>
2021-01-01 22:37:18 +0700

    Test Line Project

---
+++ src/test/java/com/dicoding/farislinejava/FarislinejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicoding.farislinejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarislinejavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
